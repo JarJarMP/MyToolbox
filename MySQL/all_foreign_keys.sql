d941717dcf55a78979d931e9c8d573bce66c3655
Péter Maróti <maroti.peter@gmail.com>
2016-03-28 10:42:27 +0200

    add - mysql - find foreign keys

---
+++ MySQL/all_foreign_keys.sql
@@ -0,0 +1,25 @@
+-- http://stackoverflow.com/questions/201621/how-do-i-see-all-foreign-keys-to-a-table-or-column
+
+-- table
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    CONSTRAINT_NAME,
+    REFERENCED_TABLE_NAME,
+    REFERENCED_COLUMN_NAME
+FROM
+    INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE
+    REFERENCED_TABLE_NAME = '<table>';
+
+-- column
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    CONSTRAINT_NAME,
+    REFERENCED_TABLE_NAME,
+    REFERENCED_COLUMN_NAME
+FROM
+    INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE
+    REFERENCED_COLUMN_NAME = '<column>';
